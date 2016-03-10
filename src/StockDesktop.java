62f3a0e5fcd8a4f9009a8885aef967037a515919
heyzzk <heyzzk@126.com>
2016-03-11 02:11:43 +0800

    homesync-get data demo

--- src/StockDesktop.java
+++ src/StockDesktop.java
@@ -1,11 +1,17 @@
 import java.awt.FlowLayout;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-
-import com.sun.org.apache.bcel.internal.generic.LLOAD;
 
 public class StockDesktop {
 	
@@ -21,6 +27,28 @@ public class StockDesktop {
 		jf.add(new JLabel("密码"));
 		jf.add(new JTextField(10));
 		jf.add(new JButton("登陆"));//创建并添加按钮
+		getdata();
+	}
+	
+	public static void getdata(){
+		URL ur = null;
+		BufferedReader reader = null;
+		try {
+			ur = new URL("http://hq.sinajs.cn/list=sh601006,sh601001");
+			HttpURLConnection uc = (HttpURLConnection) ur.openConnection();
+			reader = new BufferedReader(new InputStreamReader(ur.openStream(),"GBK"));
+			String line;
+			while ((line = reader.readLine()) != null)
+			{
+				System.out.print(line);
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
